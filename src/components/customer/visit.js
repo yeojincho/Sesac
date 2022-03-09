08578a59916218f5aaf9c9a70c97c9731aa771bb
seungyeon Lee <asfire0302@gmail.com>
2022-03-09 21:08:46 +0900

    [add]  visit 페이지 카테고리 색 변경 기능, 주소 복사 기능 추가

--- src/components/customer/visit.js
+++ src/components/customer/visit.js
@@ -1,68 +1,106 @@
-import React from "react";
+import React,  { useState, useEffect } from "react";
 import './visit.css'
+import Customer from './Customer';
 
 export default function Visit(){
-    return(
-        <div className="wrapper inner-box">
-            <div className="title">
-                <h2>고객센터</h2>
-                <h3>__오시는 길</h3>
-            </div>
-        <div className="menu">
-            <ul className="menuList">
-                <li><a href="#">공지사항</a></li>
-                <li><a href="#">문의하기</a></li>
-                <li><a href="/faq">자주 묻는 질문</a></li>
-                <li><a href="/visit">오시는 길</a></li>
-            </ul>
-        </div>{/* e:menu */}
-        <div className="subMenu">
-            <ul className="subMenuList">
-                <li><a href="#">송파러닝센터</a></li>
-                <li><a href="#">가락러닝센터</a></li>
-                <li><a href="#">문정러닝센터</a></li>
-            </ul>
-        </div>
-        <main className="visitInfo">
-            <div className="address">
-                <h3>주소 : 서울특별시 중대로 121 롯데캐슬파인힐 2층</h3>
-                <a href="#" className="share">
-                    <p>공유</p>
-                </a>
-            </div>
-            <div className="mapBox">
-                <div className="officeImg">
-                    회사 전경 사진 영역
-                </div>
-                <div className="map">
-                    지도 영역
-                </div>
-            </div>
-            
-        </main>
-        <div className="transport">
-            <div className="transportTitle">
-                <h3>교통편</h3>
-            </div>
-            <div className="transportList">
-                <div className="car">
-                    <p className="transportListTitle carTitle">승용차</p>
-                    <div className="transportListContents carContents">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.
-                    
-                    </div>                  
-                </div>
-                <div className="subway">
-                    <p className="transportListTitle subwayTitle">지하철</p>
-                    <p className="transportListContents subwatContents">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.</p>                    
-                </div>
-                <div className="bus">
-                    <p className="transportListTitle busTitle">버스</p>
-                    <p className="transportListContents busContents">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.</p>                    
-                </div>{/* e:transportList */}
-            </div>{/* e:transport */}
 
-        </div>
-        </div>
+    const [ currentClick, setcurrentClick ] = useState(null);
+    const [ prevClick, setPrevClick ] = useState(null);
+
+    const handleClick = (e) => {
+        setcurrentClick(e.target.id);
+    };
+
+    useEffect(
+        (e) => {
+        if (currentClick !== null) {
+            let current = document.getElementById(currentClick);
+            current.classList.add('subOn');
+        }
+        if (prevClick !== null) {
+            let prev = document.getElementById(prevClick);
+            prev.classList.remove('subOn');
+        }
+        setPrevClick(currentClick);                  
+        },[currentClick]); 
+
+    const copyAddress = () => {
+        const addressText = document.getElementById('addressText').textContent;
+        const textArea = document.createElement('textArea');
+        textArea.textContent = addressText;
+        document.body.append(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        textArea.remove();
+        alert('주소가 복사되었습니다.')
+    }   
+    
+
+
+    return(
+        <>
+            <div className="wrapper inner-box">
+                <div className='title'>
+                    <h2>고객센터</h2>
+                    <h3>__오시는 길</h3>
+                </div>
+                
+            <Customer />            
+                <div className="visitSubMenuBox">
+                    <ul className="visitSubMenuList">
+                        <li >
+                            <button id = "subMenu1" onClick={ handleClick }>
+                                송파러닝센터
+                            </button>                
+                        </li>
+                        <li >
+                            <button id = "subMenu2" onClick={ handleClick }>
+                            가락러닝센터
+                            </button>                
+                        </li>
+                        <li >
+                            <button id = "subMenu3" onClick={ handleClick }>
+                            문정러닝센터
+                            </button>                
+                        </li>
+                    </ul>
+                </div>{/* e: submenu */}
+                <main className="visitInfo">
+                    <div className="address">
+                        <h3 id="addressText">주소 : 서울특별시 중대로 121 롯데캐슬파인힐 2층</h3>
+                        <button className='share' onClick={copyAddress}>
+                            공유
+                        </button>
+                    </div>
+                    <div className="mapBox">
+                        <div className="officeImg">
+                            회사 전경 사진 영역
+                        </div>
+                        <div className="map">
+                            지도 영역
+                        </div>
+                    </div>        
+                </main>{/* e: visitInfo */}
+                <div className="transport">
+                        <h3>교통편</h3>
+                    <div className="transportList">
+                        <div className="car">
+                            <p className="transportListTitle carTitle">승용차</p>
+                            <p className="transportListContents carContents">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.                    
+                            </p>                  
+                        </div>
+                        <div className="subway">
+                            <p className="transportListTitle subwayTitle">지하철</p>
+                            <p className="transportListContents subwatContents">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.</p>                    
+                        </div>
+                        <div className="bus">
+                            <p className="transportListTitle busTitle">버스</p>
+                            <p className="transportListContents busContents">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, eget non scelerisque bibendum.</p>                    
+                        </div>
+                    </div>{/* e:transportList */}
+                </div>{/* e:transport */}
+            </div>
+        </>
     )
 }
