aac3d0f308651e8488923994f6573bb7d9ace55b
kalmtalyst <kalmtalyst@gmail.com>
2022-03-14 16:11:57 +0900

    [modify] App.js pull 받은 코드 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-
-import { useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import './components/common/common.css'
+import "./components/common/common.css";
 
+import MobileLayout from "./components/common/mobile/MobileLayout";
+import PCLayout from "./components/common/PCLayout";
 
-import MobileLayout from './components/common/mobile/MobileLayout';
-import PCLayout from './components/common/PCLayout';
-
-
-
-function App({sessionLogin}) {
+function App({ sessionLogin }) {
   /* 로그인 세션 확인 */
-  const [isLogin,setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
-    sessionLogin === null ?  setIsLogin(false) : setIsLogin(true);
-  })
+    sessionLogin === null ? setIsLogin(false) : setIsLogin(true);
+  });
 
   /* 반응형 웹 */
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  
+
   return (
     <div className="wrapper">
-      { isMobile ? <MobileLayout isLogin={isLogin} isMobile={isMobile} /> : <PCLayout isLogin={isLogin} isMobile={isMobile} /> }
+      {isMobile ? (
+        <MobileLayout isLogin={isLogin} isMobile={isMobile} />
+      ) : (
+        <PCLayout isLogin={isLogin} isMobile={isMobile} />
+      )}
     </div>
   );
 }
