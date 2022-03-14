0ad60926c820a683c0e4d574848e97a968ac1891
yeahhaaa <notyaeji@gmail.com>
2022-03-14 17:37:10 +0900

    [modify] edit className

--- src/components/academy/Recruit.js
+++ src/components/academy/Recruit.js
@@ -1,78 +1,80 @@
 import React from "react";
 import "./Recruit.css";
 import { Label, FormGroup, Input, Button, Form, Col } from "reactstrap";
-function Partnership() {
+function Recruit() {
   return (
-    <div className="wrap inner-box">
-      <h3 className="header">
+    <div className='recruit-wrap inner-box'>
+      <h3 className='recruit-header'>
         <b>인재채용안내</b>
       </h3>
-      <span>엑스퍼트 아카데미는 실력있는 강사님과 매니저를 선발합니다.</span>
-      <div className="Contents">
-        <img className="lectureImg" src={"/images/lecture1.png"} />
+      <span className='recruit-span'>
+        엑스퍼트 아카데미는 실력있는 강사님과 매니저를 선발합니다.
+      </span>
+      <div className='recruit-Contents'>
+        <img className='recruit-lectureImg' src={"/images/lecture1.png"} />
         <Form>
-          <FormGroup row className="TextBox">
-            <Label for="apply" sm={2} className="subTitle">
+          <FormGroup row className='recruit-TextBox'>
+            <Label for='apply' sm={2} className='recruit-subTitle'>
               지원자명
             </Label>
             <Col sm={10}>
-              <Input id="apply" name="apply" placeholder="name" type="text" />
+              <Input id='apply' name='apply' placeholder='name' type='text' />
             </Col>
           </FormGroup>
-          <FormGroup row className="TextBox">
-            <Label for="examplePassword" sm={2} className="subTitle">
+          <FormGroup row className='TextBox'>
+            <Label for='examplePassword' sm={2} className='subTitle'>
               연락처
             </Label>
             <Col sm={10}>
               <Input
-                id="examplePassword"
-                name="password"
-                placeholder="phone number"
-                type="text"
+                id='examplePassword'
+                name='password'
+                placeholder='phone number'
+                type='text'
               />
             </Col>
           </FormGroup>
-          <FormGroup row className="TextBox">
-            <Label for="examplePassword" sm={2} className="subTitle">
+          <FormGroup row className='recruit-TextBox'>
+            <Label for='examplePassword' sm={2} className='recruit-subTitle'>
               E-mail
             </Label>
             <Col sm={10}>
               <Input
-                id="examplePassword"
-                name="password"
-                placeholder="email"
-                type="email"
+                id='examplePassword'
+                name='password'
+                placeholder='email'
+                type='email'
               />
             </Col>
           </FormGroup>
-          <FormGroup row className="TextBox">
-            <Label for="examplePassword" sm={2} className="subTitle">
+          <FormGroup row className='recruit-TextBox'>
+            <Label for='examplePassword' sm={2} className='recruit-subTitle'>
               주요역량
             </Label>
             <Col sm={10}>
               <Input
-                id="examplePassword"
-                name="password"
-                placeholder="text"
-                type="text"
+                id='examplePassword'
+                name='password'
+                placeholder='text'
+                type='text'
               />
             </Col>
           </FormGroup>
-          <FormGroup row tag="fieldset" className="TextBox">
-            <legend className="col-form-label col-sm-2 subTitle">
+          <FormGroup row tag='fieldset' className='recruit-TextBox'>
+            <legend className='col-form-label col-sm-2 subTitle'>
               지원분야
             </legend>
             <Col sm={10}>
-              <FormGroup check>
-                <Input name="radio2" type="radio" /> <Label check>강사</Label>
+              <FormGroup className='recruit-FormGroup' check>
+                <Input name='radio2' type='radio' /> <Label check>강사</Label>
               </FormGroup>
               <FormGroup check>
-                <Input name="radio2" type="radio" /> <Label check>매니저</Label>
+                <Input name='radio2' type='radio' /> <Label check>매니저</Label>
               </FormGroup>
             </Col>
           </FormGroup>
-          <FormGroup row className="TextBox">
-            <Label for="checkbox2" sm={2} className="subTitle">
+          <FormGroup row className='recruit-TextBox'>
+            <Label for='checkbox2' sm={2} className='subTitle'>
               Checkbox
             </Label>
             <Col
@@ -81,7 +83,7 @@ function Partnership() {
               }}
             >
               <FormGroup check>
-                <Input id="checkbox2" type="checkbox" />{" "}
+                <Input id='checkbox2' type='checkbox' />{" "}
                 <Label check>Check me out</Label>
               </FormGroup>
             </Col>
@@ -93,7 +95,7 @@ function Partnership() {
                 size: 10,
               }}
             >
-              <Button className="submitBtn">Submit</Button>
+              <Button className='recruit-submitBtn'>Submit</Button>
             </Col>
           </FormGroup>
         </Form>
@@ -101,4 +103,4 @@ function Partnership() {
     </div>
   );
 }
-export default Partnership;
+export default Recruit;
--- src/components/mypage/Management.js
+++ src/components/mypage/Management.js
@@ -4,43 +4,23 @@ import "./Management.css";
 
 function Management() {
   return (
-    <div className='inner-box wrap1'>
-      <div className='sideBar'>
-        <ul className='info'>
-          회원정보
-          <li>
-            <button>- 회원정보관리</button>
-          </li>
-          <li>
-            <button>- 비밀번호 변경</button>
-          </li>
-        </ul>
-        <ul className='info'>
-          수강정보
-          <li>
-            <button>- 수강신청 확인/취소</button>
-          </li>
-          <li>
-            <button>- 수강 내역</button>
-          </li>
-        </ul>
-      </div>
-      <div className='contentBox'>
-        <p id='title' className='Imp'>
+    <div className='inner-box Management-wrap1'>
+      <div className='Management-contentBox'>
+        <p id='title' className='Management-Imp'>
           마이페이지
         </p>
         <p id='title' className='Imp'>
           _____님 엑스퍼트 아카데미에 오신걸 환영합니다.
         </p>
-        <div className='inform'>
-          <p className='infoTitle'>회원정보 관리</p>
-          <p className='infoTitle'>필수정보</p>
+        <div className='Management-inform'>
+          <p className='Management-infoTitle'>회원정보 관리</p>
+          <p className='Management-infoTitle'>필수정보</p>
           <p>성명 : 홍길동 </p>
           <p>아이디 : expert</p>
           <p> 성별:남</p>
-          <Form className='infoForm'>
+          <Form className='Management-infoForm'>
             <FormGroup>
-              <p className='infoTitle'>부가정보1</p>
+              <p className='Management-infoTitle'>부가정보1</p>
               <Label for='exampleEmail'>휴대전화</Label>
               <Input type='text' />
             </FormGroup>
@@ -52,15 +32,15 @@ function Management() {
               <Label for='examplePassword'> 생년월일 </Label>
               <Input type='date' id='select' />
             </FormGroup>
-            <p className='service'>메일링서비스</p>
+            <p className='Management-service'>메일링서비스</p>
             <FormGroup check>
-              <Input type='checkbox' id='checkbox' />
+              <Input type='checkbox' id='Management-checkbox' />
               수신받음
             </FormGroup>
             <FormGroup>
               <Label for='examplePassword'>회원구분 </Label>
               <Label for='exampleSelect'></Label>
-              <Input id='select' name='select' type='select'>
+              <Input id='Management-select' name='select' type='select'>
                 <option>교육담당자</option>
                 <option>일반</option>
               </Input>
@@ -68,7 +48,7 @@ function Management() {
             <FormGroup>
               <p className='infoTitle'>부가정보2</p>
               <Label for='exampleEmail'>회사명</Label>
-              <Input id='exampleEmail' name='email' type='email' />
+              <Input id='Management-exampleEmail' name='email' type='email' />
             </FormGroup>
             <FormGroup>
               <Label for='examplePassword'> 부서명 </Label>
@@ -87,7 +67,7 @@ function Management() {
               <Input id='examplePassword' name='password' type='text' />
             </FormGroup>
           </Form>
-          <div className='bottomBtn'>
+          <div className='Management-bottomBtn'>
             <Button>확인</Button>
             <Button>회원탈퇴</Button>
           </div>
