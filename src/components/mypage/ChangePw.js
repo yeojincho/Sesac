8ecdb9e016901f2b08e1b1d021449c5716dd7319
yeojin <yeojincho0129@gmail.com>
2022-03-14 15:40:56 +0900

    commit

--- src/components/mypage/ChangePw.js
+++ src/components/mypage/ChangePw.js
@@ -5,9 +5,9 @@ import "./ChangePw.css";
 function ChangePw() {
   return (
     <div>
-      <div className='inner-box wrap1'>
-        <div className='sideBar'>
-          <ul className='info'>
+      <div className="inner-box wrap1">
+        <div className="sideBar">
+          <ul className="info">
             회원정보
             <li>
               <button>- 회원정보관리</button>
@@ -16,7 +16,7 @@ function ChangePw() {
               <button>- 비밀번호 변경</button>
             </li>
           </ul>
-          <ul className='info'>
+          <ul className="info">
             수강정보
             <li>
               <button>- 수강신청 확인/취소</button>
@@ -26,30 +26,30 @@ function ChangePw() {
             </li>
           </ul>
         </div>
-        <div className='contentBox'>
-          <p className='myTitle'>마이페이지 _____님 </p>
-          <p className='myTitle'>엑스퍼트 아카데미에 오신걸 환영합니다.</p>
-          <h2 className='Imp'>비밀번호변경</h2>
+        <div className="contentBox">
+          <p className="myTitle">마이페이지 _____님 </p>
+          <p className="myTitle">엑스퍼트 아카데미에 오신걸 환영합니다.</p>
+          <h2 className="Imp">비밀번호변경</h2>
           <p>비밀번호 변경을 위해 현재 비밀번호를 입력해 주세요</p>
-          <FormGroup className='FormNow'>
-            <Label for='exampleEmail' className='Imp'>
+          <FormGroup className="FormNow">
+            <Label for="exampleEmail" className="Imp">
               현재비밀번호
             </Label>
-            <Input type='text' />
+            <Input type="text" />
           </FormGroup>
-          <FormGroup className='FormNow'>
-            <Label for='examplePassword' className='Imp'>
+          <FormGroup className="FormNow">
+            <Label for="examplePassword" className="Imp">
               {" "}
               새로운 비밀번호{" "}
             </Label>
-            <Input type='text' />
+            <Input type="text" />
           </FormGroup>
-          <FormGroup className='FormNow'>
-            <Label for='examplePassword' className='Imp'>
+          <FormGroup className="FormNow">
+            <Label for="examplePassword" className="Imp">
               {" "}
               새로운 비밀번호 확인{" "}
             </Label>
-            <Input type='text' className='inputEnd' />
+            <Input type="text" className="inputEnd" />
           </FormGroup>
           <Button>확인</Button>
         </div>
