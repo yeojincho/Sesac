66d72a14e5620c357b5a7c9ad8b7bc531a1531ef
yeojin <yeojincho0129@gmail.com>
2022-03-22 00:42:49 +0900

    [ modify ] 십년감수

--- src/components/mypage/ AccountWithdrawal.js
+++ src/components/mypage/ AccountWithdrawal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Label, FormGroup, Input, Button } from "reactstrap";
+import { Button, FormGroup } from "reactstrap";
 import "./AccountWithdrawal.css";
 import HeadTitle from "./HeadTitle";
 import SideMenu from "./SideMenu";
@@ -13,7 +13,6 @@ function AccountWithdrawal() {
         <div className="flex-box">
           <SideMenu />
           <div className="account-contentBox">
-            {/* <p className="account-Imp account-title">회원탈퇴</p> */}
             <h2 className="pageTitle">회원탈퇴</h2>
             <p className="account-subtitle">
               회원탈퇴시 고객님의 회원정보는 모두 삭제 됩니다.
@@ -23,27 +22,39 @@ function AccountWithdrawal() {
               반영하겠습니다.
             </p>
             <div className="account-userinfo">
-              <p className="account-Imp"> 성명: {user1.name}</p>
-              <p className="account-Imp"> 아이디: {user1.id}</p>
+              <p className="account-Imp">
+                {" "}
+                성명: <span>{user1.name}</span>
+              </p>
+              <p className="account-Imp">
+                {" "}
+                아이디: <span>{user1.id}</span>
+              </p>
             </div>
 
-            <FormGroup className="form">
-              <Label for="exampleText" className="account-Imp withdrwal-reason">
-                탈퇴사유
-              </Label>
-              <Input
-                className="account-FormText"
-                id="exampleText"
-                name="text"
-                type="textarea"
-                placeholder="간단하게 사유를 작성해 주세요
-              예시: 이용안함 / 서비스 불만족 / 재가입"
+            <form className="form">
+              <h3>탈퇴사유</h3>
+              <div class="form-floating">
+                <textarea
+                  class="form-control"
+                  placeholder="간단하게 사유를 작성해 주세요."
+                  id="floatingTextarea2"
+                  style={{ height: "200px" }}
+                ></textarea>
+                <label for="floatingTextarea2">
+                  예시: 이용안함 / 서비스 불만족 / 재가입
+                </label>
+              </div>
+            </form>
+            <form className="form">
+              <h3>탈퇴확인을 위한 비밀번호 입력</h3>
+              <input
+                className="form-control"
+                id="password-check"
+                type="password"
+                placeholder="비밀번호를 입력하세요"
               />
-            </FormGroup>
-            <FormGroup className="form">
-              <Label for="exampleEmail">탈퇴확인을 위한 비밀번호 입력</Label>
-              <Input className="account-FormPw" type="text" />
-            </FormGroup>
+            </form>
             <Button className="form-btn">탈퇴하기</Button>
           </div>
         </div>
