a5eb447a918d716d9c4bbe1b7b505c1f6337f942
kalmtalyst <kalmtalyst@gmail.com>
2022-03-23 17:27:44 +0900

    [add] 고객센터 문의글보기 페이지 추가

--- src/components/customer/QuestionList.js
+++ src/components/customer/QuestionList.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Table } from "reactstrap";
 
 function QuestionList() {
   const questions = useSelector((state) => state.questionReducer.questions);
-
   return (
     <div>
-      <Table striped className="question-list-table">
+      <Table className="question-list-table">
         <thead>
           <tr>
             <th>No</th>
@@ -16,17 +16,58 @@ function QuestionList() {
             <th>작성일</th>
           </tr>
         </thead>
+
         <tbody>
-          {questions.map((question) => (
-            <tr>
-              <th scope="row">{question.id}</th>
-              <td>{question.title}</td>
-              <td>{question.writer}</td>
-              <td className="question-date" style={{ textAlign: "center" }}>
-                {question.date}
+          {questions.length > 0 ? (
+            questions.map((question) => (
+              <tr key={question.id}>
+                <th scope="row" style={{ width: "10vw", lineHeight: "5vh" }}>
+                  {question.id}
+                </th>
+                <Link
+                  to={{
+                    pathname: `/question/:${question.id}`,
+                    state: {
+                      title: question.title,
+                      writer: question.writer,
+                      content: question.content,
+                      date: question.date,
+                    },
+                  }}
+                >
+                  <td style={{ width: "60vw" }}>{question.title}</td>
+                </Link>
+                <td style={{ width: "10vw", lineHeight: "5vh" }}>
+                  {question.writer}
+                </td>
+                <td
+                  className="question-date"
+                  style={{
+                    textAlign: "center",
+                    width: "10vw",
+                    lineHeight: "5vh",
+                  }}
+                >
+                  {question.date}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr style={{ textAlign: "center" }}>
+              <td></td>
+              <td
+                style={{
+                  textAlign: "center",
+                  height: "8vh",
+                  lineHeight: "8vh",
+                }}
+              >
+                등록된 문의가 없습니다.
               </td>
+              <td></td>
+              <td></td>
             </tr>
-          ))}
+          )}
         </tbody>
       </Table>
     </div>
