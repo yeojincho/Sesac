c9c0446a6a2461be1ef66930d581100b3e59fc41
hansol787897465121 <gnu671@naver.com>
2022-03-23 15:46:18 +0900

    [modify] 메인 배너 수정

--- src/components/main/popup/Login.js
+++ src/components/main/popup/Login.js
@@ -4,18 +4,25 @@
 
 
 import React from 'react';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import $ from 'jquery';
 
 import './login.css';
 
 
 export default function Login({ open, close }) {
-   /* 로그인 Btn */
-   const [errorMsg, setErroMsg] = useState("");
-   const loginFn = () => {
+  /* 아이디 저장 */
+  const [userIdSave, setUserIdSave] = useState(false);
+  const [cookies, setCookie] = useCookies(['userIdCookie']);
+  const userIdCookie = cookies.userIdCookie;
+  
+  /* 로그인 Btn */
+  const [errorMsg, setErroMsg] = useState("");
+  const [userId, setUserId] = useState("");
+  const loginFn = () => {
       // 유효성 검사
-      if($('.id-box input').val() === ''){
+      if(userId === ''){
         setErroMsg("아이디를 입력해주세요.");
         return;
       }else if($('.pw-box input').val() === ''){
@@ -24,11 +31,22 @@ export default function Login({ open, close }) {
       }else{
         setErroMsg("");
       }
+      
+      //아이디 저장 체크시 쿠키생성
+      if(userIdSave){
+        setCookie('userIdCookie', userId , { 
+          path: '/' ,
+        });
+      }
 
       // 로그인 세션 생성
       sessionStorage.setItem('user_id', 'test');
       document.location.href = '/';
-   }
+  }
+
+
+  
+   
 
 
    return (
@@ -41,7 +59,7 @@ export default function Login({ open, close }) {
               <h1 className="pop-title"><strong>로그인</strong></h1>
               <div className="login-box">
                   <div className="id-box">
-                    <input type="text" className="inp-box" placeholder="아이디" />
+                    <input type="text" className="inp-box" placeholder="아이디" value={userIdCookie !== null ?  userIdCookie : ""} onChange={(e) =>setUserId(e.target.value)}/>
                   </div>
                   <div className="pw-box">
                     <input type="password" id="pwLblGd" className="inp-box" placeholder="비밀번호" />
@@ -50,7 +68,7 @@ export default function Login({ open, close }) {
               </div>
               <div className="login-chk-box">
                 <div className="inp-chk-box">
-                  <input type="checkbox" id="chkChrGd" className="inp-chk" />
+                  <input type="checkbox" id="chkChrGd" className="inp-chk" onChange={(e) =>setUserIdSave(e.target.checked)}/>
                   <label htmlFor="chkChrGd">아이디 저장</label>
                 </div>
                 <button className="btn-login" onClick={loginFn}>로그인</button>
